2e610483c69d8015328ce48ec19be67df9a0d5e7
yortuc <yortucboylu@gmail.com>
2019-10-27 09:50:46 +0100

    bck: add touch controls

--- backGame/index.js
+++ backGame/index.js
@@ -2,6 +2,7 @@ import { PORTAL } from './constants.js'
 import { maps } from './maps.js'
 import { renderGame } from './mapRenderer.js'
 import { tryMovePlayer, moveEnemies } from './player.js'
+import startSwipeGestures from './touchControls.js'
 
 const c =  document.getElementById("c").getContext("2d")
 
@@ -12,6 +13,7 @@ let startTime = null
 let elapsedTime = 0
 let pathWays = [[]]
 
+// keyboard controls
 document.addEventListener('keydown', function checkKey(e) {
   let move = null
   if(event.keyCode === 37) move = {x: -1} // left
@@ -20,18 +22,29 @@ document.addEventListener('keydown', function checkKey(e) {
   if(event.keyCode === 38) move = {y: -1} // up
   
   if(!move) return
+  makeMove(move)
+})
 
+function makeMove(move) {
   tryMovePlayer(currentMap, move, (currentCellId, targetCellId) => {
       
-      pathWays = moveEnemies(currentMap)
-    
-      if(targetCellId === PORTAL){ // moved on portal -> change the currentMap
-        setTimeout(function(){ 
-          mapIndex += 1
-          currentMap = maps[mapIndex]
-        }, 500);
-      }
+    pathWays = moveEnemies(currentMap)
+  
+    if(targetCellId === PORTAL){ // moved on portal -> change the currentMap
+      setTimeout(function(){ 
+        mapIndex += 1
+        currentMap = maps[mapIndex]
+      }, 500);
+    }
   })
+}
+
+// touch controls
+startSwipeGestures({
+  'left': ()=> makeMove({x: -1}),
+  'right':()=> makeMove({x: 1}),
+  'down': ()=> makeMove({y: 1}),
+  'up':   ()=> makeMove({y: -1})
 })
 
 function step(timestamp) {
